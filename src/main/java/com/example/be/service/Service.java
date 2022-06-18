ce536d36251c8518286405328d0f8b96b7851eaf
Iulia26 <ifilimon26@gmail.com>
2022-06-18 13:33:35 +0300

    first commit

---
+++ src/main/java/com/example/be/service/Service.java
@@ -0,0 +1,11 @@
+package com.example.be.service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+
+@org.springframework.stereotype.Service
+public class Service {
+
+}
